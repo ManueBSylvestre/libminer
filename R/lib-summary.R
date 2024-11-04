3903efe4efe27233c29996b7b73c759ae2f989aa
Manuelle Beaudry-Sylvestre <manuelle.beaudry-sylvestre@dfo-mpo.gc.ca>
2024-11-04 15:03:25 -0500

    added documentation for lib_summary function

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,9 +1,29 @@
+#' Provide Number of R Packages by Library
+
+#'
+
+#' Provide the number of R package by library in
+
+#'  a data.frame
+
+#'
+
+#' @return a data.frame of R packages by library
+
+#' @export #somethingthat makes the function available to your user
+
+#'
+
+#' @examples
+#' lib_summary
+#'
 lib_summary <- function() {
   #what packages are installed in my system in a big messy presentation
   pkgs <- utils::installed.packages()
   #we pull out the libpath
   pkg_tbl <- table(pkgs[, "LibPath"])#table = nb of rows in libpath by unique values in libpaths
   #make a df
+
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   #new names
   names(pkg_df) <- c("Library", "n_packages")
