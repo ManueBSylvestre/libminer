32b7646c11c30e796bd085fa3d5a23812affc93f
Manuelle Beaudry-Sylvestre <manuelle.beaudry-sylvestre@dfo-mpo.gc.ca>
2024-11-04 13:42:04 -0500

    add libsummary to pkg

---
+++ R/lib-summary.R
@@ -0,0 +1,15 @@
+lib_summary <- function() {
+  #what packages are installed in my system in a big messy presentation
+  pkgs <- utils::installed.packages()
+  #we pull out the libpath
+  pkg_tbl <- table(pkgs[, "LibPath"])#table = nb of rows in libpath by unique values in libpaths
+  #make a df
+  pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
+  #new names
+  names(pkg_df) <- c("Library", "n_packages")
+  pkg_df #return that package df
+}#return fct= convention is to only use this fct when want to return something early in the function (do it consistently)
+
+
+
+
